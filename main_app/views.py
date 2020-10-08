08769f94e35cf3d5840dfbd20fb3bf707fb68613
Frankie Barreto <barretoacquire@gmail.com>
2020-10-08 07:23:43 -0700

    created cities views, added path

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 
 def home(request):
     return render(request, 'base.html')
+
+def cities(request):
+    return render(request, 'cities.html')

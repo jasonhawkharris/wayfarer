dacc9e16c0fbc2e5fcd5337a6d927f8b59e5f97b
jasonhawkharris <jasonhawkharris@gmail.com>
2020-10-08 09:19:28 -0700

    renders login form to the login-modal

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,2 +1,9 @@
 from django.forms import ModelForm
+from django.contrib.auth.models import User
 from .models import Profile
+
+
+class Login_Form(ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password']
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from .forms import Login_Form
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'base.html')
+    login_modal = Login_Form()
+    context = {
+        'login_form': login_modal
+    }
+    return render(request, 'base.html', context)

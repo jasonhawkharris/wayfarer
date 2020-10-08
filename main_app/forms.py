3ada7bf0eac33c6fdf249b8c906d71297e88aa2b
jasonhawkharris <jasonhawkharris@gmail.com>
2020-10-08 10:26:50 -0700

    Login, logout, signup complete. Base.html complete (for now)

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,6 +1,20 @@
 from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Profile
+
+
+class User_Form(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username',
+                  'email', 'password1', 'password2']
+
+
+class Profile_Form(ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['hometown', 'photo']
 
 
 class Login_Form(ModelForm):

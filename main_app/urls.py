33a3909698f0ae34997912c09f337477bd160c98
jasonhawkharris <jasonhawkharris@gmail.com>
2020-10-09 15:12:44 -0700

    adds a home button to the sidebar.

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('cities/', views.cities, name='cities'),
     path('cities/<int:city_id>/', views.city_detail, name='detail'),
     path('posts/add_post', views.add_post, name='add_post'),
-    # path('posts/form', views.form, name='form'),
+    path('posts/form', views.form, name='form'),
     path('posts/', views.posts, name='posts'),
     path('accounts/register/', views.register, name='register'),
 ]

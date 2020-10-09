cfcc17e1931830472945926d3de87c546d7c80bd
andrewjspivey <12spivey@gmail.com>
2020-10-09 14:58:04 -0700

    resolving conflict

--- main_app/models.py
+++ main_app/models.py
@@ -14,7 +14,6 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.username
 
-<<<<<<< HEAD
 
 """ @receiver(post_save, sender=User)
 def create_profile(sender, **kwargs):
@@ -22,8 +21,6 @@ def create_profile(sender, **kwargs):
         profile = Profile.objects.create(user=kwargs['instance'])
 
 post_save.connect(create_profile, sender=User) """
-=======
->>>>>>> 5332a240cb4651c99d1aa79451d585be1fe8f103
 
 
 @receiver(post_save, sender=User)
